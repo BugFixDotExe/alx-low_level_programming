b0091b58b997843cf30e556fb8d3c0d0ce4b3942
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-14 23:55:16 -0800

    missed a space

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	char buffer[1024];
 
-	strcpy(buffer, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n")
-		;
+	strcpy(buffer, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			);
 	write(2, buffer, strlen(buffer));
 
-	return (0);
+	return (1);
 
 }
