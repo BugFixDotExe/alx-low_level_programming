95ad93bb088cda8b45f655b2dd169da894981e9b
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-14 20:28:09 -0800

    change %d to %zu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,20 +13,20 @@ int main(void)
 	if (sizeof(void *) == 4)
 	{
 		/* working with a 32bit Architecture*/
-		printf("Size of a char: %zu byte(s)", sizeof(char));
-		printf("Size of an int: %zu byte(s)", sizeof(int32_t));
-		printf("Size of a long int: %zu byte(s)", sizeof(long int));
-		printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+		printf("Size of an int: %zu byte(s)\n", sizeof(int32_t));
+		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 		printf("Size of a float: %zu byte(s)", sizeof(float));
 	}
 
 	else if (sizeof(void *) == 8)
 	{
-		printf("Size of a char: %zu byte(s)", sizeof(char));
-		printf("Size of an int: %zu byte(s)", sizeof(int));
-		printf("Size of a long int: %zu byte(s)", sizeof(long int));
-		printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+		printf("Size of an int: %zu byte(s)\n", sizeof(int));
+		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 		printf("Size of a float: %zu byte(s)", sizeof(float));
-	}
+	i}
 	return (0);
 }
