e5ff3cbcacf2979448646f189a333479775dde0c
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-14 23:23:15 -0800

    Solved the Advanced problem 🔥🔥🥳🥳🤞🏾🤞🏾🤞🏾

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+
+/**
+ * main - entry point of the program
+ * Return: zero
+ */
+
+#include <errno.h>
+#include <stdio.h>
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+
+}
