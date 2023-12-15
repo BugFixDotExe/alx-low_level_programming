cca603299528c6f2cd80870130a16cd0f18fc7d2
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-14 18:10:48 -0800

    Solutuon to problem 5 testing time 🤞🏾

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+
+/**
+ * main - entry point
+ * Return: zero
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
