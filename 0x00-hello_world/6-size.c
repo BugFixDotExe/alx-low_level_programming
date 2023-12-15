147bf093f26fb84dfb1544f1d946f3d73bcaa287
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-14 20:24:57 -0800

    change %d to %zu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,13 +16,13 @@ int main(void)
 		printf("Size of a char: %zu byte(s)", sizeof(char);
 		printf("Size of an int: %zu byte(s)", sizeof(int32_t);
 		printf("Size of a long int: %zu byte(s)", sizeof(long int);
-		printf("Size of a long long int: %zu byte(s)" sizeof(long long int);
+		printf("Size of a long long int: %zu byte(s)", sizeof(long long int);
 		printf("Size of a float: %zu byte(s)", sizeof(float);
 	}
 
 	else if (sizeof(void *) == 8)
 	{
-		printf("Size of a char: %zu byte(s)" sizeof(char);
+		printf("Size of a char: %zu byte(s)", sizeof(char);
 		printf("Size of an int: %zu byte(s)", sizeof(int));
 		printf("Size of a long int: %zu byte(s)", sizeof(long int));
 		printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
