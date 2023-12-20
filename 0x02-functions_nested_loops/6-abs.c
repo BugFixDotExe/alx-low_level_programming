aff38bbbdf6e171275a69850429490993581e0a5
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-20 05:42:26 -0800

    Betty go BRRRR 🔥🔥🔥

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _abs -  a function that computes the absolute value of an integer.
- * @int: the int to be worked upon
- * Return: the absolute value of the int
+ * _abs - a function that computes the absolute value of an integer.
+ * @a: the int to be worked upon
+ * Return: the absolute value of a
  */
 int _abs(int a)
 {
