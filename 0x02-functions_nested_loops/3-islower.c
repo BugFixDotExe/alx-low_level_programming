d42ddb229954e261dd5113d9558e1ba1c2c0b062
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-20 04:37:32 -0800

    Fixed Betty styling error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,12 @@
 
 /**
  * _islower - a function that checks for lowercase character.
+ * @c: the character to be checked.
  * Return: 1 ifislower and 0 ifnotlower
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z' )
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
