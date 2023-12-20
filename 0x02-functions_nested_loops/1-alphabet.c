333f389825be2c0d1030a9ffb7f5516478e8c251
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-20 03:46:52 -0800

    Solution to alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * print_alphabet - a function that prints the alphabet,
+ * in lowercase, followed by a new line
+ * Return : void
+ */
 void print_alphabet(void)
 {
 
 	int i;
+
 	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
