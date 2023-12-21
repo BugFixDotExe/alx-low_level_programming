5845a79f42575a53ae78d933dacc1c691e771a24
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-21 08:24:31 -0800

    This is the solution to the upper case check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: the int to be checked.
+ * Return: 1 isUpper and 0 is!Upper 😏😏
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
