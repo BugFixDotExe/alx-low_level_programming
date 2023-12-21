5fe6210c137b3d922d8505cca69b5dffc18dea75
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-21 08:37:33 -0800

    Solution to multiplying two multiplicand 🤓

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - a function that * two ints.
+ * @a: first int.
+ * @b: second int.
+ * Return: solution to the multiplicand. 🤓🤓
+ */
+
+int mul(int a, int b)
+{
+	int output;
+
+	output = a * b;
+
+	return (output);
+}
+
