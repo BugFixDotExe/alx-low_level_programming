c9544198876d3a3d388fd2366db67920348f6ae4
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-21 00:42:32 -0800

    solution to printing to 98 depending on the condition

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_to_98 - a function that prints all natural number up till 98
+ * @n: the argument to be handled.
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+
+	if (n == 98)
+		printf("%d\n", n);
+	if (n == 0 || n < 0)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(", ");
+			++n;
+		}
+		printf("\n");
+	}
+
+	if (n > 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(", ");
+			--n;
+		}
+		printf("\n");
+	}
+}
