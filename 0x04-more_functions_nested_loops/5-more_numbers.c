108d52311ab62deddb37b91caed63c05fd0591e5
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-23 23:40:04 -0800

    Solution to printing from the range 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 0-14 10X.
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for(i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j <= 9)
+				end(j);
+			else if(j > 9)
+			{
+				start(j);
+				end(j);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+void start(int i)
+{
+	_putchar('0' + i / 10);
+}
+
+void end(int i)
+{
+	_putchar('0' + i % 10);
+}
