667e80a43af6c5c49a1fb091f55c41b97cbda7d3
BugFixDotExe <bugfix.exe@gmail.com>
2023-12-27 23:34:55 -0800

    Solution to printing the times table 🥳🥳🥳

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,18 +13,31 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			mul = j * i;
-			_putchar('0' + mul / 10);
-			_putchar('0' + mul % 10);
-
-			if (j != 9)
+			mul = i * j;
+			if ((i * j) > 9)
+			{
+				double_digit(mul);
+			}
+			else
+				_putchar('0' + mul);
+			if (j <= 8)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-
 		}
 		_putchar('\n');
 	}
 }
 
+/**
+ * double_digit - a function that gets the start and end
+ * of a digit
+ * @input: an integer of type int
+ * Return: void
+ */
+void double_digit(int input)
+{
+	_putchar('0' + input / 10);
+	_putchar('0' + input % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,4 +17,5 @@ int add(int, int);
 void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
+void double_digit(int input);
 #endif /*MAIN_H*/
