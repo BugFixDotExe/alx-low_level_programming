bee88f401ef08b26b8eeb9566f62ef2afda3d44e
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-04 11:54:07 -0800

    Betty go brrrr 🔥

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Write a function that takes a pointer to an int 
+ * reset_to_98 - Write a function that takes a pointer to an int
  * as parameter and updates the value it points to to 9
  * @n: an int variable
  * Return: Void
