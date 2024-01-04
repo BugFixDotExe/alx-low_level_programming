db182dc45d37c258918b44c7b02efc01f6f4f11f
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-04 14:33:05 -0800

    devugging

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -19,11 +19,10 @@ void print_rev(char *s)
 
 	for (str_len = 0; *(s +str_len) != '\0'; str_len++)
 		;
-	while (str_len > 0)
+	while (str_len >= 0)
 	{
 		_putchar(*(s +str_len));
 		str_len--;
 	}
-	_putchar('\n');
 }
 
