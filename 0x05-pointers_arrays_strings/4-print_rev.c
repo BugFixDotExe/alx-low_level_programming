43dfe0ff6b6b4564980a1d280da259704f8836d7
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-04 14:28:16 -0800

    solution to the reverse the aSIMOVE STRING

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+
+
+/**
+ * print_rev - a function that reverses a string
+ * @s: the string to be reversed
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int str_len;
+	
+
+	if (s == NULL)
+		return;
+
+	for (str_len = 0; *(s +str_len) != '\0'; str_len++)
+		;
+	while (str_len >= 0)
+	{
+		_putchar(*(s +str_len));
+		str_len--;
+	}
+	_putchar('\n');
+}
+
