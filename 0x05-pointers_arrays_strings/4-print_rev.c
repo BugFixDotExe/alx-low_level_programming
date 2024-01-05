971a1eee649104ce0f154f22e88c1ddfff9dcebf
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-05 14:05:32 -0800

    debugging session

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,21 +12,29 @@ void print_rev(char *s)
 {
 	int str_len;
 	
+	str_len = -1;
+
+	if (s == NULL)
+		return;
+
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	if (s == NULL)
-		return;
 
-	for (str_len = 0; s[str_len] != '\0'; str_len++)
-		;
+	while (*s)
+	{
+		str_len++;
+		s++;
+	}
+	s--;
+	
 	while (str_len >= 0)
 	{
-		if (s[str_len] == '\0')
-			continue;
-		_putchar(s[str_len]);
+		_putchar(*s);
+		--s;
+		--str_len;
 	}
 	_putchar('\n');
 }
