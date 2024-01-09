5b47b75c6723ceeebc3d6f94cb8d17355f561edc
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-09 13:33:55 -0800

    Solution finding a character within a string 🔥🤞🏾 TEST TIME !

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: the complete string to perform a search on.
+ * @c: the character to be found.
+ * Return: a pointer to the first occurence of the c if found
+ */
+char *_strchr(char *s, char c)
+{
+	if (s == NULL)
+		return (NULL);
+
+	while (*s)
+	{
+		if (*s == c)
+			return (*(&s));
+		s++;
+	}
+	return (NULL);
+}
+
