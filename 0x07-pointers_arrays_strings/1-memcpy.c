7cb01c6f998096a8ab79327c1a93ff60d60e7255
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-09 13:15:52 -0800

    solution to the memcpy problem 🔥🥳

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy -  a function that copies memory area.
+ * @dest: final destination of copied memory
+ * @src: location to copy from
+ * @n: the number of bytes to copy
+ * Return: a pointer to new memory of type char
+ * FYI: memcpy copies data not freaking memory
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int  i;
+
+	if (dest == NULL || src == NULL)
+		return dest;
+	if (n <= 0)
+		return dest;
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
+
