a44dc678949d2df3bd5ac9c92fd13b37d09adef2
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-11 08:23:15 -0800

    solution to find the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,16 +9,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
 
-	i = 0;
-	if (s == NULL || *s == '\0')
-	{
-		return(i);
-	}
+	if (*s == '\0')
+		return (0);
 	else
-		return (1);
-	
-	i += _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
 
