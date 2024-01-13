7c5ecbb3380343280a80de780e986e0cd428c79d
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-13 09:53:22 -0800

    solution to printing out the number of args 🫡

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: the number of args
+ * @argv: an arry of args
+ * Return: 0 on success
+ */
+
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
