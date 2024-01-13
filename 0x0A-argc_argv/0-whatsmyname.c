185301a5a3e9e20128d4fa9ce9fe27977a13370b
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-13 09:48:18 -0800

    solution to printing out the name of the program using the argv and argc 🤓🤓😶‍🌫️

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - the entry point
+ * @argc: the number of args
+ * @argv: an array of argunments
+ * Return: 0 on success
+ */
+
+int main(int __attribute__((unused))argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
+
