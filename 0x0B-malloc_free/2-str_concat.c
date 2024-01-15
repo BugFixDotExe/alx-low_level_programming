6384c048151f81fb524c6599b624e2699a974a80
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-15 12:10:01 -0800

    bugdix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,24 +8,27 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char* concat;
+	char *concat;
 	int len_s1, len_s2;
 	int i, j;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	else if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	for (len_s1 = 0; *(s1 + len_s1) != '\0'; len_s1++)
 		;
 	for (len_s2 = 0; *(s2 + len_s2) != '\0'; len_s2++)
-		;	
+		;
 	concat = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (concat == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < len_s1; i++)
 		*(concat + i) = *(s1 + i);
 	for (j = 0; j < len_s2; j++)
