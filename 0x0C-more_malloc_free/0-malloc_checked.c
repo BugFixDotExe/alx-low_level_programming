117c07a957c410443641dcc12015781d24fea8f8
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-18 00:23:36 -0800

    Solution to problem 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that acclocates memory
+ * @b: size of memory
+ * Return: Void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+		exit(98);
+	return (mem);
+}
+
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define NULL 0
+
+#include <stdlib.h>
+void *malloc_checked(unsigned int b);
+
+#endif /*MAIN_H*/
