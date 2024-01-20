296635dc1207a75c011002876270e5f14372f9e6
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-20 01:51:41 -0800

    solution got

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ * @dest: the destination string.
+ * @src: the origin string.
+ * @n: the number of bytes to copy.
+ * Return: a pointer the new dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+		;
+
+	if (n > i)
+	{
+
+		for (i = 0; i < n && *(src + i) != '\0'; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
+		*(dest + i) = '\0';
+	}
+	if (n < i)
+	{
+		for (i = 0; i < n; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
+	}
+	return (dest);
+}
+
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,6 +5,7 @@
  * @ptr: a pointer to the pervious memory.
  * @old_size: old size of ptr.
  * @new_size: the new size.
+ * Return: a pointer to new memory with ptr content.
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -25,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (new_mem == NULL)
 			return (NULL);
 		for (i = 0; i < old_size; i++)
-			*((char*)new_mem + i) = *((char*)ptr + i);
+			*((char *)new_mem + i) = *((char *)ptr + i);
 		free(ptr);
 		return (new_mem);
 	}
@@ -35,10 +36,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		new_mem = malloc(new_size);
 		if (new_mem == NULL)
 			return (NULL);
-		else
+		else if (new_mem != NULL)
 		{
 			for (i = 0; i < new_size; i++)
-				*((char*)new_mem + i) = *((char*)ptr + i);
+				*((char *)new_mem + i) = *((char *)ptr + i);
 		}
 	}
 
