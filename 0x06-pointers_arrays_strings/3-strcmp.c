d214aaf0e6085a9f518ecde44ae40012a27fe960
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-20 08:35:12 -0800

    changed the return

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,16 +17,16 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 < *s2)
-			return (-1);
+			return (-13);
 		else if (*s1 > *s2)
-			return (1);
+			return (13);
 		s1++;
 		s2++;
 	}
 	if (*s1 == '\0' && *s2 != '\0')
-		return (-1);
+		return (-13);
 	else if (*s1 != '\0' && *s2 == '\0')
-		return (1);
+		return (13);
 	return (0);
 }
 
