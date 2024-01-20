d9411a18dadb99397e061518e5abd06d68314eaa
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-19 22:52:32 -0800

    fixed the betty styling problem

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 
 /**
  * _calloc - a function that allocates memory for an array
- * @nmeb: number of elements
+ * @nmemb: number of elements
  * @size: the number of bytes for each element in the arr
  * Return: void.
  */
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mem = malloc (size * nmemb);
+	mem = malloc(size * nmemb);
 	if (mem == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size / sizeof(size); i++)
