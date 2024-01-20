d2681f6aaae71ae706c67621c98b698411cb6a9a
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-20 08:32:32 -0800

    revised version of strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,42 +9,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-	int min;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		;
-	for (j = 0; *(s2 + j) != '\0'; j++)
-		;
-
-	if (*(s1 + 0) == '\0' && j > 0)
-		return (15);
-	if (i > 0 && *(s2 + 0) == '\0')
-		return (15);
-	if (*(s1 + 0) == '\0' && *(s2 + 0) == '\0')
-		return (0);
-
-	if (i > 0 && j > 0)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (i < j)
-			min = i;
-		else
-			min = j;
-		for (i = 0; i < min; i++)
-		{
-			if (*(s1 + i) == *(s2 + i))
-				continue;
-			if (*(s1 + i) < *(s2 + i))
-				return (-15);
-			if (*(s1 + i) > *(s2 + i))
-				return (15);
-		}
+		if (*s1 < *s2)
+			return (-1);
+		else if (*s1 > *s2)
+			return (1);
+		s1++;
+		s2++;
 	}
+	if (*s1 == '\0' && *s2 != '\0')
+		return (-1);
+	else if (*s1 != '\0' && *s2 == '\0')
+		return (1);
 	return (0);
 }
 
