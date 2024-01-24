7dc8c7a9d7cc2b61570583d991c1606d6baa1ff2
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-24 02:30:34 -0800

    solution to betty style error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,20 +4,23 @@
 /**
  * new_dog - a function that creates a new dog
  * @name: name of the dog
- * @age age of the dog
+ * @age: age of the dog
  * @owner: owner of the dog
  * Return: a pointee to the new dog struct
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	if (name == NULL  || owner == NULL)
 		return (NULL);
+
 	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
-       	if (new_dog->name == NULL)
+	if (new_dog->name == NULL)
 		free(new_dog);
 	strcpy(new_dog->name, name);
 
@@ -32,3 +35,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	return (new_dog);
 }
+
