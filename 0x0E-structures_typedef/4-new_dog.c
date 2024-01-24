4a998b1635a7b7cd993b3429def87d24b4d68de8
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-24 02:24:50 -0800

    freed the memory incase of a nULL

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,9 +17,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL  || owner == NULL)
 		return (NULL);
 	new_dog->name = malloc(sizeof(char) * strlen(name) + 1);
-       	strcpy(new_dog->name, name);
+       	if (new_dog->name == NULL)
+		free(new_dog);
+	strcpy(new_dog->name, name);
 
 	new_dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+	}
 	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
