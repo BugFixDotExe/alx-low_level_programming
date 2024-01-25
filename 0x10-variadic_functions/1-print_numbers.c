cdaa5f705beb869f3197f3b3e0bfda4bdcc7dab3
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-25 06:46:40 -0800

    debug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,6 +15,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	if (n == 0)
 		return;
+	if (separator == NULL)
+		separator = "";
 	for (i = 0; i < n; i++)
 	{
 		if (i < (n - 1))
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_all - a function that prints anything
+ * @format: a str for format
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+}
