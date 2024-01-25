17870a53840320ae037bdc17427b05373ab34e20
BugFixDotExe <bugfix.exe@gmail.com>
2024-01-25 03:58:45 -0800

    solution to the problem of adding the digits to get the sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+
+/**
+ * sum_them_all - a function that returns the sum of all it's parameters
+ * @n: a constant of type unsigned int
+ * Return: the sum of all the variable args
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+
+	va_list args;
+	va_start(args, n);
+	sum = 0;
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	return (sum);
+
+}
