4f6354da8c72a6e8a4db8ecdf1b3e47bacda282a
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-01 12:22:20 -0800

    loosing 5bytes

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
+/**
+ * free_list - a function that frees the nodes
+ * @head: the start of the list
+ * Return: void
+ */
 void free_list(list_t *head)
 {
+	list_t *prev;
+	list_t *next;
 
+	prev = head;
+	while (prev != NULL)
+	{
+		next = prev->next;
+		free(prev->str);
+		free(prev);
+		prev = next;
+	}
 }
