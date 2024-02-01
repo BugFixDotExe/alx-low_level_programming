ccf0db4d4e5ce357a7e99e0ca785761056461703
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-01 03:34:44 -0800

    solution to adding a node to the start

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ * list_len - a function that returns the number of elements in a linked list_t list.
+ * @h: the pointer to the list of type list_t
+ * Return: the number of elements in a list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		printf("[0] (nil)");
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
+
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - a function that adds a new node
+ * at the beginning of a list_t list.
+ * @head: a pointer of a pointer to the original pointer.
+ * @str: the str to be duplicated
+ * Return: a pointer to the new node
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head;
+
+	new_head = malloc(sizeof(list_t));
+	if (new_head == NULL || str == NULL)
+		return (NULL);
+	new_head->str = malloc(sizeof(char) * strlen(str));
+	if (new_head->str == NULL)
+		return (NULL);
+	new_head->str = strdup(str);
+	new_head->len = strlen(str);
+	new_head->next = *head;
+	*head = new_head;
+	return (*head);
+}
