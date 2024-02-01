8a3a363f14c0f68744213d00153e6e8920ef2f50
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-01 02:31:46 -0800

    SOlution to the print and showing the len of the nodes in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: a pointer of type list_t
+ * Return: the len of the node(s) in the list
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	if (h == NULL)
+		printf("[0] (nil)");
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		i += 1;
+		h = h->next;
+	}
+	return (i);
+}
