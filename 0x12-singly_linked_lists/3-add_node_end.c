391abd17dbf9c9a29780cc1981a495d9e5e4f5f6
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-02 06:45:25 -0800

    debugging

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,38 @@
 #include "lists.h"
 
+
+/**
+ * add_node - add a node when head is null
+ * @head: the start of the list
+ * @str: the string to be copied into the node
+ * Return: a pointer to the created node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *end_node;
+
+	end_node = malloc(sizeof(list_t));
+	if (end_node == NULL)
+		return (NULL);
+	end_node->str = malloc(sizeof(char) * strlen(str));
+	if (end_node->str == NULL)
+	{
+		free(end_node);
+		return (NULL);
+	}
+	end_node->str = strdup(str);
+	end_node->len = strlen(str);
+	end_node->next = NULL;
+	*head = end_node;
+	return (*head);
+}
+
 /**
  * add_node_end - a function that adds a new node at the end of a list_t list.
  * @head: a pointer to a pointer of a pointer
  * @str: the string to be worked upon
  * Return: a pointer of type list_t
  */
-
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *end_node;
@@ -16,22 +41,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (str == NULL)
 		return (NULL);
 	if (*head == NULL)
-	{
-		end_node = malloc(sizeof(list_t));
-		if (end_node == NULL)
-			return (NULL);
-		end_node->str = malloc(sizeof(char) * strlen(str));
-		if (end_node->str == NULL)
-		{
-			free(end_node);
-			return (NULL);
-		}
-		end_node->str = strdup(str);
-		end_node->len = strlen(str);
-		end_node->next = NULL;
-		*head = end_node;
-		return (*head);
-	}
+		return (add_node(head, str));
 
 	while (*nodes)
 	{
