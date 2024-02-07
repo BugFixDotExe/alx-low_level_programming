26b3c932b6f308bb24a285864c106c3257048904
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-06 23:51:46 -0800

    solution to adding a node when the pointer to the head is nULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	i = 0;
 	inner_head = *head;
-	if (*head == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	if (idx == 0)
+
+	if (*head == NULL && idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+		*head = new_node;
+		return (*head);
+	}
+	if (idx == 0 && *head != NULL)
 	{
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
