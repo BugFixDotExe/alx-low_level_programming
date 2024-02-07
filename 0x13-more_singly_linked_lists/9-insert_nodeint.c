b5cfa1465df9a8ebd832afa24b4d7b5f37d7fecb
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-06 23:46:40 -0800

    added the solution for adding a node to the start of the ;ist

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (new_node == NULL)
 			return (NULL);
 		new_node->n = n;
-		new_node->next = *head;
+		new_node->next = inner_head;
 		*head = new_node;
+		return (*head);
 	}
 	while (inner_head)
 	{
