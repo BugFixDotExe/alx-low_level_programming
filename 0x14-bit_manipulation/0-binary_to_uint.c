009cbec7daa03211099ca2d7b80c04ea2c4a676c
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-08 08:16:15 -0800

    solution to the freaking int to binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_uint - a function that converts a binary
+ * number to an unsigned int.
+ * @b: the int that has to converted
+ * Return: the binary of b
+ */
+
+int pow_base(int n)
+{
+	int square;
+	square = 1;
+	while (n > 0)
+	{
+		square *= 2;
+		n--;
+	}
+	return (square);
+}
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len, sum, i;
+
+	sum = 0;
+	i = 0;
+
+	for (len = 0; *(b + len) != '\0'; len++);
+	len = len - 1;
+
+	while (len >= 0)
+	{
+		if (*(b + i) != '1' && *(b + i) != '0')
+			return (0);
+		if (*(b + i) == '1')
+			sum += pow_base(len);
+		i++;
+		len--;
+	}
+	return (sum);
+}
