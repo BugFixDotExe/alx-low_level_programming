dc39b7dc0a84856a14c6fce5320ce56d6b346a80
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-09 02:15:56 -0800

    solution to returning the bit at an index once it is turned into its binary form, without using malloc, or an array

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
- *
+ * get_bit - a function that returns
+ * the value of a bit at a given index.
+ * @n: the binary number to be converted
+ * @index: at what index to get the binary number
+ * Return: the value at that index
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int i, r;
+
+	for (i = 0; n > 0; i++)
+	{
+		r = n % 2;
+		if (i == index)
+			return (r);
+		n = n / 2;
+	}
+	return (-1);
 }
