9655318185accf4b4952052bf42f863808586ed1
bugfix.exe <bugfix.exe@gmai.com>
2024-03-23 04:48:53 -0700

    debugging

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = current_node->next;
 	new_node->prev = current_node;
-	current_node->next->prev = new_node;
+	if (current_node->next != NULL)
+		current_node->next->prev = new_node;
 	current_node->next = new_node;
 	return (new_node);
 }
