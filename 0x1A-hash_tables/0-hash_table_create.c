118cafb56566b3e49e11d2f05763b295eebfb296
bugfix.exe <bugfix.exe@gmai.com>
2024-04-13 05:26:00 -0700

    code style checkup and fixing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,16 +13,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned int i;
 
+	/*This will allocate size enough to hold the struct members*/
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL || size == 0)
 		return (NULL);
 	hash_table->size = size;
+	/*This will aloocate mem enough to hold n items of type hash_table_t*/
 	hash_table->array = malloc(hash_table->size * sizeof(hash_table_t));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
+	/*This is setting all the slots in the arr to NULL until needed*/
 	for (i = 0; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 	return (hash_table);
