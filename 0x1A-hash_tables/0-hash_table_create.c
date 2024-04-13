0d9110419805ca231c3af25a25cd9714f4f9457c
bugfix.exe <bugfix.exe@gmai.com>
2024-04-13 04:46:01 -0700

    solution to the problem of 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hash table
+ * given size
+ *
+ * @size: the size of the table at creation
+ * Return: a pointer the hashtable of type hash_table_t
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t) * size);
+	if (hash_table == NULL || size <= 0)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = NULL;
+	return (hash_table);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -29,4 +33,6 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
 #endif
