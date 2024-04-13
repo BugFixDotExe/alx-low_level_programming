a14c7b796a458ee851b27b30eae24c08ab49feb2
bugfix.exe <bugfix.exe@gmai.com>
2024-04-13 04:56:38 -0700

    debugging

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table == NULL || size == 0)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = calloc(sizeof(hash_table_t) * size);
+	hash_table->array = malloc(sizeof(hash_table_t) * size);
 
 	for (i = 0; i < hash_table->size; i++)
 	{
