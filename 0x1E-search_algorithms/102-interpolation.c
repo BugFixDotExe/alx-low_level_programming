263a4a4be89b775145a2b2da85a7ac81ca13e158
bugfix.exe <bugfix.exe@gmail.com>
2024-07-17 15:21:50 +0100

    debugged the interpolation error

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * interpolation_search - a function that searches
  * for a value in a sorted array of integers using
@@ -12,34 +11,40 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t lower_bound, higher_bound;
-	int mid;
+	size_t mid;
 
 	lower_bound = 0;
 	higher_bound = size - 1;
 
 	if (array == NULL)
 		return (-1);
+
 	while (lower_bound <= higher_bound && value >= array[lower_bound] && value <= array[higher_bound])
 	{
-		mid = lower_bound + (
-				higher_bound - lower_bound) * ((
-						value - array[lower_bound]) / (
-							array[higher_bound] - array[lower_bound]));
+		/* Prevent division by zero */
+		if (array[higher_bound] == array[lower_bound])
+		{
+			if (array[lower_bound] == value)
+				return lower_bound;
+			else
+				break;
+		}
+		mid = lower_bound + ((higher_bound - lower_bound) * (value - array[lower_bound])) / (array[higher_bound] - array[lower_bound]);
+		/*ensure mid within the bound of size*/
+		if (mid >= size)
+			break;
+
+		printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
+
 		if (value == array[mid])
 		{
-			printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+			printf("Found %d at index: %ld\n", value, mid);
 			return (mid);
 		}
 		if (value < array[mid])
-		{
-			printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
 			higher_bound = mid - 1;
-		}
 		else
-		{
-			printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
 			lower_bound = mid + 1;
-		}
 	}
 	return (-1);
 }
